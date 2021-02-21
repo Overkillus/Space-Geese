a64037f62e92b41c7f8e7128e105b60f0226bba5
wweronika <wiesiolekweronika@gmail.com>
2021-02-21 10:57:46 +0100

    poems cleaned

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from re import search
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import pronouncing
+import random
 
 
 
@@ -29,8 +30,20 @@ def get_results_cows():
     # print(results)
     return results
 
+def get_results_chickens():
+    url = "https://www.bbc.com/news/topics/cqlyx9q32g8t/chickens"
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    # print(html)
+    soup = BeautifulSoup(html, "html.parser")
+    results = soup.find_all("span", {"class": "lx-stream-post__header-text"})
+    # print(results)
+    return results
 
-    
+
+def get_random_result(results):
+    return [random.choice(results)]
+
 sents = []
 goose_words = ["goose", "geese", "Goose", "Geese", "gosling", "Gosling", "goslings", "Goslings", "goose's", "geese's", "Goose's", "Geese's"]
 
@@ -60,7 +73,8 @@ def find_first_noun(words):
 
 def goosify(sent):
     l = sent.split()
-    for i in len(l):
+    print(l)
+    for i in range(len(l)):
         if l[i] == "cow's":
             l[i] = "goose's"
         elif l[i] == "Cow's":
@@ -73,7 +87,18 @@ def goosify(sent):
             l[i] = "Geese"
         elif l[i] == "cows":
             l[i] = "geese"
-    return " ".join(l)
+            # chicks
+        elif l[i] == "chickens" or l[i] == "chicks":
+            l[i] = "geese"
+        elif l[i] == "Chickens" or l[i] == "Chicks":
+            l[i] = "Geese"
+        elif l[i] == "chicken":
+            l[i] = "goose"
+        elif l[i] == "hens":
+            l[i] = "geese"
+        elif l[i] == "Chicken":
+            l[i] = "Goose"
+    return l
 
 def results_to_words(results):
     l = []
@@ -85,10 +110,12 @@ def results_to_words(results):
 def cow_results_to_words(results):
     l = []
     for r in results:
+        print(r.text)
+        # txt = r.text.replace('''):
+         
         r_words = goosify(r.text)
         l.append(r_words)
     return l
-
 
 
 def find_sents_and_adjs(r_words_list):
@@ -115,28 +142,40 @@ def find_sents_and_adjs(r_words_list):
     # print(nouns)
     return sents, adjs, nouns
 
-def goosify(sent):
-    l = sent.split()
-    for i in range(len(l)):
-        if l[i] == "cow's":
-            l[i] = "goose's"
-        elif l[i] == "Cow's":
-            l[i] = "Goose's"
-        elif l[i] == "cow":
-            l[i] = "goose"
-        elif l[i] == "Cow":
-            l[i] = "Goose"
-        elif l[i] == "Cows":
-            l[i] = "Geese"
-        elif l[i] == "cows":
-            l[i] = "geese"
-    return l
+# def goosify(sent):
+#     l = sent.split()
+#     for i in range(len(l)):
+#         if l[i] == "cow's":
+#             l[i] = "goose's"
+#         elif l[i] == "Cow's":
+#             l[i] = "Goose's"
+#         elif l[i] == "cow":
+#             l[i] = "goose"
+#         elif l[i] == "Cow":
+#             l[i] = "Goose"
+#         elif l[i] == "Cows":
+#             l[i] = "Geese"
+#         elif l[i] == "cows":
+#             l[i] = "geese"
+#     return l
 
 
 
-# l1 = results_to_words(get_results())
-# l2 = cow_results_to_words(get_results_cows())
+def get_random_stem_set():
+    r1 = get_results()
+    r1 = results_to_words(r1)
+    r2 = get_results_cows()
+    r2 = cow_results_to_words(r2)
+    r3 = get_results_chickens()
+    r3 = cow_results_to_words(r3)
+    r = r1 + r2 + r3
+    result = get_random_result(r3)
+    sents, adjs, nouns = find_sents_and_adjs(result)
+    print("PRINTING RESULTS IN SCAPER")
+    print(sents, adjs, nouns)
+    if not (sents == [] or (adjs == [None] and nouns == [None])):
+        return sents, adjs, nouns
+    else: 
+        return get_random_stem_set()
 
-# for s in l2:
-#     find_sents_and_adjs(s)
 
