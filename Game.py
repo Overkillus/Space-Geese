d28a83e22317de955f8646b81bc7b53ac82af36c
wweronika <wiesiolekweronika@gmail.com>
2021-02-21 02:15:39 +0100

    Fixed infinite recursion bug on win

--- Game.py
+++ Game.py
@@ -175,12 +175,18 @@ class Game:
                     self.enemies[i] = None
 
                     # spawn letter on hit
-                    random_letter = self.get_random_letter()
-                    text_object_rect = get_text_rect(random_letter[0], (255,0,0), e.center[0], e.center[1])
-                    text_object_rect.append(random_letter[0])
-                    text_object_rect.append(random_letter[1])
-                    self.letters.append(text_object_rect)
-                    break
+                    all_revealed = True
+                    for b in self.is_letter_revealed:
+                        if not b:
+                            all_revealed = False
+                            break
+                    if all_revealed:
+                        random_letter = self.get_random_letter()
+                        text_object_rect = get_text_rect(random_letter[0], (255,0,0), e.center[0], e.center[1])
+                        text_object_rect.append(random_letter[0])
+                        text_object_rect.append(random_letter[1])
+                        self.letters.append(text_object_rect)
+                        break
 
         # Move the letters down
         for l in self.letters:
